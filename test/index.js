a11bc744ebd44f740c5a9a4e76c9879d968e0d2c
curbengh <43627182+curbengh@users.noreply.github.com>
2019-07-25 00:15:24 +0930

    refactor: es6-fy (#15)

--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var should = require('chai').should(); // eslint-disable-line
+const should = require('chai').should(); // eslint-disable-line
 
-describe('hexo-renderer-haml', function() {
-  var r = require('../lib/renderer');
+describe('hexo-renderer-haml', () => {
+  const r = require('../lib/renderer');
 
-  it('default', function() {
-    var result = r({
+  it('default', () => {
+    const result = r({
       text: '%div Hello #{name}'
     }, {
       name: 'Hexo'
